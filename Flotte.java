9573e8ecaa5ba5105bb4c864e1f8861310a79773
thomascormier <43412314+thomascormier@users.noreply.github.com>
2019-04-29 16:49:23 +0200

    Update Flotte.java

--- Flotte.java
+++ Flotte.java
@@ -1,37 +1,45 @@
-import java.util.ArrayList;
-import java.util.Collection;
+ /*
 
-public class Message{
-
-    private Robot envoyeur;
-    private Robot destinataire;
-    private String contenu;
-
-    private Collection<MessageListener> MessageListeners = new ArrayList<>();
-
-    public void resut
+Elle joue aussi un rôle dans la communication, car tous les messages passent par cette classe Flotte.
+    Càd : quand un message est créé, il est capté par la classe flotte, car il est listener de Message
 
 
-    public Message(Robot envoyeur,  Robot destinataire, String contenu) {
-        this.envoyeur = envoyeur;
-        this.destinataire = destinataire;
-        this.contenu = contenu;
+Elle a aussi une interface graphique, qui affiche tous les robots disponibles, avec leur position.
+
+Elle permet d’afficher les communications échangées : lors de la réception d’un message de la part d’un robot,
+ce message est retransmis aux autres robots.
+
+ */
+
+ import java.util.ArrayList;
+
+ public class Flotte implements MessageListener{
+
+    ArrayList<Robot> flotte = new ArrayList<>();
+
+
+    private void addRobot(Robot robot){
+        flotte.add(robot);
     }
 
-    public String getContenu(){
-        return this.contenu;
+    private void removeRobot(Robot robot){
+        flotte.remove(robot);
     }
 
 
 
+    //    Elle joue aussi un rôle dans la communication, car tous les messages passent par cette classe Flotte.
+    //    Càd : quand un message est créé, il est capté par la classe flotte, car il est listener de Message
 
-    public void addMessageListener(MessageListener listener) {
-        MessageListeners.add(listener);
+    @Override
+     public void onMessage(Message message){
+        // Lorsqu'un message est envoyé, il est envoyé à tout le monde
+        for (int i = 0; i < flotte.size(); i++)
+            new Message(flotte[i].getNom(),message.getContenu());
+
     }
 
-    public void removeMessageListener(MessageListener listener) {
-        MessageListeners.remove(listener);
-    }
+
 
 
 }
